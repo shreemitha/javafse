e46e93d7c70dc2d70c9d2a56eb22e8ba2e2cbfe1
shreemitha <shreemithak01@gmail.com>
2025-06-23 10:17:41 +0530

    Add files via upload

---
+++ week 1/design patterns/ex-2/WordDocument.java
@@ -0,0 +1,9 @@
+
+// File: WordDocument.java
+public class WordDocument implements Document {
+    @Override
+    public void open() {
+        System.out.println("Opening Word document.");
+    }
+}
+
